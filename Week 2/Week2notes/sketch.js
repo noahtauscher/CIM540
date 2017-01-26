7828f555c3e7c8964a1c0850b63ae4166d45b9f0
noahtauscher <NoahTauscher@Noahs-MacBook-Pro-2.local>
2017-01-26 15:33:34 -0500

    Week2Notes

--- Week 2/Week2notes/sketch.js
+++ Week 2/Week2notes/sketch.js
@@ -1,7 +1,24 @@
-function setup() {
+var mouthX = 100;
+var mouthY = 270;
+var mouthW = 300;
+var mouthH = 100;
 
+function setup() {
+	createCanvas(500,500);
+	background(20,100,100);
+	
+	console.log("width: " + width + " " + "height:" + height);
 }
 
 function draw() {
-  
+	ellipse(170,150,75,75);
+	
+	ellipse(320, 150, 75, 75);
+	
+	rect(mouthX, mouthY, mouthW, mouthH);
+	
+	line(mouthX, mouthY + mouthH/2 , ,320);
 }
+
+
+//elipse dimensions are x location, y location, width, height
