60f1f4ccbef8b323c51da8c58ddd60fe83244ab3
noahtauscher <nxt93@miami.edu>
2017-02-02 12:39:05 -0500

    save

--- Week 2/Week2Assignment/sketch.js
+++ Week 2/Week2Assignment/sketch.js
@@ -1,7 +1,29 @@
-function setup() {
+var mouthX = 342;
+var mouthY = 180;
+var mouthW = 50;
+var mouthH = 250;
 
+function setup() {
+	createCanvas(500,500);
+	background(20,100,100);
+	
+	console.log("width: " + width + " " + "height:" + height);
 }
 
 function draw() {
-  
+	ellipse(370,100,75,75);
+	
+	rect(mouthX/8,mouthY - 130,mouthW,mouthH*1.5);
+	
+	rect(mouthX/1.5,mouthY - 130,mouthW,mouthH*1.5)
+	
+	rect(mouthX/3.8,mouthY,mouthH - 111,mouthW - 4);
+	
+	
+	
+	rect(342, mouthY, mouthW, mouthH);
+	
 }
+
+
+//elipse dimensions are x location, y location, width, height
