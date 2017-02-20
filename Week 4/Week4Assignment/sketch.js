cb70ae9bab913bcad82931a88d4d6fff62c825c1
noahtauscher <nxt93@miami.edu>
2017-02-20 14:05:51 -0500

    Week 3 Assignment

--- Week 4/Week4Assignment/sketch.js
+++ Week 4/Week4Assignment/sketch.js
@@ -3,45 +3,27 @@ var mouthY = 180;
 var mouthW = 50;
 var mouthH = 250;
 
-var elipseX = 370;
-var elipseY = 100;
-var elipseW = 75;
-var elipseH = 75;
-
-
-
 function setup() {
 	createCanvas(500,500);
-	backgroundColor(0,0,100);
+	background(20,100,100);
 	
 	console.log("width: " + width + " " + "height:" + height);
 }
 
 function draw() {
-
+	ellipse(370,100,75,75);
 	
-		ellipse(elipseX,elipseY,elipseW,elipseH);
-	fill(0,0,0);
-	ellipse(elipseX,elipseY,elipseW/2,elipseH/2);
-	
-	fill(0,0,225);
 	rect(mouthX/8,mouthY - 130,mouthW,mouthH*1.5);
 	
 	rect(mouthX/1.5,mouthY - 130,mouthW,mouthH*1.5)
 	
 	rect(mouthX/3.8,mouthY,mouthH - 111,mouthW - 4);
 	
+	
+	
 	rect(342, mouthY, mouthW, mouthH);
 	
 }
-
-function mousePressed(){
-	
-	
-	
-}
-
-
 
 
 //elipse dimensions are x location, y location, width, height
---
+++ Week 4/Week4notes/sketch.js
@@ -0,0 +1,30 @@
+
+function setup() {
+	
+	createCanvas(400,400);
+	eColor = color(235,122,79);
+}
+
+function draw() {
+	background(255);
+	
+	fill(eColor);
+	ellipse(width/2, height/2, 200, 200);
+	
+	if(mouseX > width/2){
+		eColor = color(200,26,15);
+	}
+	
+	else{
+		eColor = color(123,45,222);
+	}
+	
+}
+
+function mousePressed(){
+  backgroundColor(123,45,222);
+}
+
+function mouseReleased(){
+  backgroundColor(255);
+}
