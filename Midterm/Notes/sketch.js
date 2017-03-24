0b107181c5c95e0f7cfcb2230421ff6b9e2136ce
noahtauscher <nxt93@miami.edu>
2017-03-24 00:05:28 -0400

    Midterm

--- Midterm/Notes/sketch.js
+++ Midterm/Notes/sketch.js
@@ -1,44 +1,56 @@
+var section1 = 100;
+var section2 = 200;
+var section3 = 300;
 
-var img1;
-var img2;
-var img3;
-var img4;
-var img5;
+var currentSection = 0;
 
-var picArray = [];
-
-var picIndex = 0;
+var fillColor;
 
 function setup() {
-    createCanvas(2558, 1428);
-
-
-    picArray.push(loadImage("images/img1.jpg"));
-    picArray.push(loadImage("images/img2.jpg"));
-    picArray.push(loadImage("images/img2.jpg"));
-    picArray.push(loadImage("images/img4.jpg"));
-    picArray.push(loadImage("images/image5.jpg"));
-
-    picIndex = picChooser(0,4);
+    createCanvas(400, 400);
+    fillColor = color(255,255,255);
 }
 
 function draw() {
-    
 
-    image(picArray[picIndex], 0, 0);
 
-   
+    background(255);
+    fill(fillColor);
+    rect(0, 0, 399, 399);
+    line(section1, 0, section1, height);
+    line(section2, 0, section2, height);
+    line(section3, 0, section3, height);
 
+
+
+    if (mouseX > 0 && mouseX <= section1) {
+        //console.log("In Section1")
+        currentSection = 1;
+    } else if (mouseX > section1 && mouseX <= section2) {
+        currentSection = 2;
+
+    } else if (mouseX > section2 && mouseX <= section3) {
+        currentSection = 3;
+
+    } else if (mouseX > section3 && mouseX <= width) {
+        currentSection = 4;
+
+    }
+
+    console.log(currentSection);
 }
 
-function mousePressed(){
-    picIndex = picChooser(0,2);
-}
+function mousePressed() {
+    if (currentSection == 1) {
+        fillColor = color(255, 0, 0)
+    } else if (currentSection == 2) {
+        fillColor = color(0, 255, 0)
 
-function picChooser(min, max) {
-   
+    } else if (currentSection == 3) {
+        fillColor = color(0, 0, 255)
 
-    var randomNum = int(random(min, max));
+    } else if (currentSection == 4) {
+        fillColor = color(100, 100, 10)
 
-    return randomNum;
+    }
 }
